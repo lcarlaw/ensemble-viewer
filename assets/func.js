4367e5421f9d06b758980ecc85f240653b273dd7
Lee Carlaw <leecarlaw@gmail.com>
2020-10-27 17:45:48 +0000

    Updates to Dash application

--- assets/func.js
+++ assets/func.js
@@ -4,8 +4,7 @@ window.local = Object.assign({}, window.local, {
             return {
                 fillColor: feature.properties.fill,
                 color: 'black',
-                opacity: 0,
-                fillOpacity: 0.55,
+                opacity: 0.7,
                 stroke: true,
             };
         }
